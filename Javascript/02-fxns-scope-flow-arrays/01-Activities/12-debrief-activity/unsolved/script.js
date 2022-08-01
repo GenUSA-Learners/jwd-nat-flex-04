51e1ff2deb3c0f3aefa878cbe23f2004ddfa7133
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-08-01 14:19:29 -0400

    Add js-2 activities

---
+++ Javascript/02-fxns-scope-flow-arrays/01-Activities/12-debrief-activity/unsolved/script.js
@@ -0,0 +1,19 @@
+console.log("js file linked!");
+// Write Functions using declaration, expression, and arrow function syntax
+
+// Write your code for Challenge 1 below this line
+// Using Function declaration syntax
+
+
+
+
+
+// Write your code for Challenge 2 below this line
+// Using Function expression syntax
+
+
+
+
+
+// Write you code for Challenge 3 below this line
+// Using ES6 Arrow function syntax
