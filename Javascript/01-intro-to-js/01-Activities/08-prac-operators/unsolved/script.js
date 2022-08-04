4d4af57a60e97a0d331639a39e8d97b4af5589c3
Ryesung <rmcdonald7@gmail.com>
2022-08-04 15:03:28 -0300

    temporary file

--- Javascript/01-intro-to-js/01-Activities/08-prac-operators/unsolved/script.js
+++ Javascript/01-intro-to-js/01-Activities/08-prac-operators/unsolved/script.js
@@ -8,10 +8,10 @@ var expression1 = (b === e);
 var expression2 = (e < d);
 
 // Use comparison operators so all expressions below log to the console as true
-console.log(a === b);
-console.log(b !== e);
-console.log(c < b);
-console.log(d > 0);
+console.log(a == b);
+console.log(b == e);
+console.log(c > b);
+console.log(d >= 0);
 
 // Use logical operators so all expressions below log to the console as true
 console.log(expression1 && expression2);
