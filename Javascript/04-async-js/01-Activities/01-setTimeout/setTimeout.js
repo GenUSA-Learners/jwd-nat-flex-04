be700d7e3ddac8b90b35d17c6be2483ea311bea7
Michael O'Connor <oconnor97@aol.com>
2022-08-16 14:28:26 -0400

    uploading code for setTimeout and setInterval

---
+++ Javascript/04-async-js/01-Activities/01-setTimeout/setTimeout.js
@@ -0,0 +1,15 @@
+// setTimeout
+setTimeout(() => {
+  console.log("callback 2 fired");
+}, 5000);
+
+setTimeout(() => {
+  console.log("callback 1 fired");
+}, 2000);
+
+// setInterval
+const greet = () => {
+  console.log("Hello World");
+};
+
+setInterval(greet, 2000);
