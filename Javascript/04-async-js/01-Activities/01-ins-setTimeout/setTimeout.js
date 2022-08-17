aacaa7c654cbeb6913d022bd90c3a99cc75ecb98
Michael O'Connor <oconnor97@aol.com>
2022-08-17 10:46:44 -0400

    finializing async js lessons

--- Javascript/04-async-js/01-Activities/01-ins-setTimeout/setTimeout.js
+++ Javascript/04-async-js/01-Activities/01-ins-setTimeout/setTimeout.js
@@ -1,11 +1,13 @@
 // setTimeout
 setTimeout(() => {
+  console.log("callback 1 fired");
+}, 2000);
+
+
+setTimeout(() => {
   console.log("callback 2 fired");
 }, 5000);
 
-setTimeout(() => {
-  console.log("callback 1 fired");
-}, 2000);
 
 // setInterval
 const greet = () => {
